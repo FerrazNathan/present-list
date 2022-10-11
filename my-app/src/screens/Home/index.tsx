c57b759e5aca213adde821e759c4b486154b4d28
Nathan Ferraz <nathanferraz@hotmail.com>
2022-10-11 14:56:04 -0300

    add input and button

--- my-app/src/screens/Home/index.tsx
+++ my-app/src/screens/Home/index.tsx
@@ -1,15 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 import { styles } from './styles';
 
 export default function Home() {
+
+  function handleParticipantAdd() {
+    console.log('Clicou')
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Jesus</Text>
-      <Text style={styles.eventDate}>Meu amor maior</Text>
+      <Text style={styles.eventName}>Célula</Text>
+      <Text style={styles.eventDate}>Sábado 16 de outubro de 2022</Text>
       <StatusBar style="auto" />
+
+      <TextInput
+        style={styles.input}
+        placeholder='Nome do participante'
+        placeholderTextColor='#6B6B6B'
+      />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleParticipantAdd}
+      >
+        <Text style={styles.buttonText}>
+          +
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
