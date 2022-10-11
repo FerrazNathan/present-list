61c8cc5bdb074e6fda38e878b2c7d29fc7bfd0f3
Nathan Ferraz <nathanferraz@hotmail.com>
2022-10-11 12:04:06 -0300

    organização e estrutura do projeto

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,30 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Home from "./src/screens/Home";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Jesus</Text>
-      <Text style={styles.eventDate}>Meu amor maior</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24,
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginTop: 40
-  },
-  eventDate: {
-    color: '#6B6B6B',
-    fontSize: 16,
-  },
-});
+
