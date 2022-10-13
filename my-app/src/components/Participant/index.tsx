793b0746931e7e39cd88155cf29f726e68f676c6
Nathan Ferraz <nathanferraz@hotmail.com>
2022-10-13 15:40:02 -0300

    criação do componente Participant

---
+++ my-app/src/components/Participant/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+
+import { styles } from "./styles";
+
+type Props = {
+  name: string
+}
+
+export default function Participant({ name }: Props) {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{name}</Text>
+      <TouchableOpacity
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>
+          -
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
--- my-app/src/screens/Home/styles.ts
+++ my-app/src/screens/Home/styles.ts
@@ -17,25 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: '#1f1e25',
     borderRadius: 5,
     color: '#FFF',
     padding: 16,
     fontSize: 16,
-    marginTop: 10,
+    marginRight: 10,
   },
   button: {
     width: 56,
     height: 56,
     borderRadius: 5,
     backgroundColor: '#31CF67',
-    marginTop: 10,
   },
   buttonText: {
     color: '#FFF',
     fontSize: 20,
     textAlign: 'center',
     marginTop: 15,
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 30,
+    marginBottom: 40,
   },
 });
